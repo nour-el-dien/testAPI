0587576086c6d39748e2ad247668aa2d4bc9e694
mohammed.mahmoud <152339587+nour-el-dien@users.noreply.github.com>
2025-10-21 15:36:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let x = new XMLHttpRequest()
+
+
+x.open("GET", "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=7c59ca07324c4ab2bae434589b8bcbe3");
+
+x.send()
+
+x.addEventListener('readystatechange',function(){
+    console.log(x.readyState);
+     if (x.readyState == 4 && x.status == 200) {
+        allData = JSON.parse(x.response).articles;
+        
+        display()
+  
+        } 
+})
+
+
+function display(list){
+    let cartona = `` ;
+        for (let i = 0; i < allData.length; i++) {
+        cartona += `<div class="col-md-4">
+                <div class="item">
+                    <img src="${allData[i].urlToImage}" class="w-100" alt="">
+                    <h3>${allData[i].title}</h3>
+                </div>
+            </div>`
+    }
+    document.querySelector("#myData").innerHTML = cartona
+
+}
